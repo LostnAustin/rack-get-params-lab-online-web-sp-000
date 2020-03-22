6bf333b1294eb4017c8ab55d6dcba968c50b67f8
Chad Lengefeld <chadlengefeld@gmail.com>
2020-03-22 19:29:35 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -30,7 +30,7 @@ binding.pry
           item = req.params["item"]
         if @@items.include?(item)
           @@cart << item
-          resp.write "#{item}"
+          resp.write "added #{item}"
         else nil || !@@items.include?(item)
           resp.write "Error"
         end
